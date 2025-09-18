88391dd5c2b55d48a201ca24d93cd23fb296ecd1
salik beigh <beighsalik51@gmail.com>
2025-09-18 10:49:00 +0530

    i

--- src/data/sahabas.js
+++ src/data/sahabas.js
@@ -1,6 +1,9 @@
 import nightJourney from "../assets/The Night Journey.png";
 import abuBakar from "../assets/abu bakar.png";
 import umar from "../assets/umar.png";
+import ali from "../assets/ali.png";
+import khadija from "../assets/khadija.png";
+
 const sahabasData = [
   {
     id: "sahaba-abu-bakr-1",
@@ -36,8 +39,9 @@ const sahabasData = [
     date: "Mar 17, 599 CE",
     content:
       "Ali ibn Abi Talib (may Allah be pleased with him) was the cousin and son-in-law of Prophet Muhammad, known as 'Asadullah' (Lion of Allah) for his exceptional courage and bravery.\n\nFrom a young age, Ali showed remarkable devotion to Islam. When the Prophet needed someone to sleep in his bed during the night of Hijra (to deceive the assassins), it was young Ali who volunteered without hesitation, knowing the great danger involved.\n\nAli's courage was legendary in battle. During the Battle of Badr, he fought with such bravery that the angels themselves were amazed. His sword, Dhul Fiqar, became famous throughout the Muslim world.\n\nBut Ali's courage wasn't just physical - it was also moral and spiritual. He stood for justice even when it was difficult, and his knowledge of the Quran and Islamic teachings was profound. His famous saying 'I am the slave of Allah and the brother of His Messenger' reflects his humility despite his great status.\n\nAli's life teaches us that true courage comes from faith in Allah and standing up for what is right, regardless of the consequences.",
-    image: nightJourney,
+    image: ali,
   },
+
   {
     id: "sahaba-khadija-1",
     name: "Khadija",
@@ -48,19 +52,7 @@ const sahabasData = [
     date: "Nov 19, 619 CE",
     content:
       "Khadija bint Khuwaylid (may Allah be pleased with her) was the first wife of Prophet Muhammad and the first person to accept Islam. Her support and sacrifice for the cause of Islam were unparalleled.\n\nKhadija was a successful businesswoman known for her wisdom and integrity. When she heard about the character of young Muhammad, she proposed marriage to him. Their marriage was one of the most blessed in Islamic history.\n\nWhen the Prophet received his first revelation and was shaken by the experience, it was Khadija who comforted him and assured him of his prophethood. She was the first to believe in his message and supported him through the most difficult times.\n\nKhadija used her wealth to support the early Muslim community, helping the poor and needy converts. She stood by the Prophet during years of persecution in Mecca, never wavering in her faith or support.\n\nHer death was a great loss to the Prophet, who called the year of her passing 'the Year of Sorrow.' Khadija's life exemplifies the importance of supporting one's spouse in their mission and using one's resources for the sake of Allah.",
-    image: nightJourney,
-  },
-  {
-    id: "sahaba-khadija-1",
-    name: "Khadija",
-    title: "The Mother of the Believers",
-    chapter: "Support and Sacrifice",
-    shortDescription:
-      "Khadija's unwavering support and sacrifice for the Prophet and Islam.",
-    date: "Nov 19, 619 CE",
-    content:
-      "Khadija bint Khuwaylid (may Allah be pleased with her) was the first wife of Prophet Muhammad and the first person to accept Islam. Her support and sacrifice for the cause of Islam were unparalleled.\n\nKhadija was a successful businesswoman known for her wisdom and integrity. When she heard about the character of young Muhammad, she proposed marriage to him. Their marriage was one of the most blessed in Islamic history.\n\nWhen the Prophet received his first revelation and was shaken by the experience, it was Khadija who comforted him and assured him of his prophethood. She was the first to believe in his message and supported him through the most difficult times.\n\nKhadija used her wealth to support the early Muslim community, helping the poor and needy converts. She stood by the Prophet during years of persecution in Mecca, never wavering in her faith or support.\n\nHer death was a great loss to the Prophet, who called the year of her passing 'the Year of Sorrow.' Khadija's life exemplifies the importance of supporting one's spouse in their mission and using one's resources for the sake of Allah.",
-    image: nightJourney,
+    image: khadija,
   },
 ];
 

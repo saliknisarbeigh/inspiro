d244ff2d6319832ba7358db91532a85f1d49de28
salik beigh <beighsalik51@gmail.com>
2025-09-19 19:10:31 +0530

    Final test before Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
